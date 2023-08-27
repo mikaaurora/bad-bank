65be4236318aff54ee41621727374a1fdfa37ab4
Mika Salazar <m.razalas@gmail.com>
2023-08-27 15:08:51 -0400

    setup forms for withdraw, deposit, and login

--- deposit.js
+++ deposit.js
@@ -1,5 +1,67 @@
-function Deposit(){
+function Deposit() {
+  const [show, setShow] = React.useState(true)
+  const [status, setStatus] = React.useState('')
+  const [balance, setBalance] = React.useState('100')
+  const [deposit, setDeposit] = React.useState('')
+  const ctx = React.useContext(UserContext)
+
+  function validate(field, label) {
+    if (!field) {
+      setStatus('Error: ' + label)
+      setTimeout(() => setStatus(''), 3000)
+      return false
+    }
+    return true
+  }
+
+  function handleDeposit() {
+    if (!validate(deposit, 'deposit')) return
+    ctx.users.push({ balance: 100 })
+    setShow(false)
+  }
+
+  function clearForm() {
+    setDeposit('')
+    setShow(true)
+  }
+
   return (
-    <h1>Deposit</h1>
+    <Card
+      bgcolor="primary"
+      header="Deposit"
+      status={status}
+      body={
+        show ? (
+          <>
+            Balance: {balance}
+            <br />
+            <br />
+            <input
+              type="input"
+              className="form-control"
+              id="deposit"
+              placeholder="Deposit Amount"
+              value={deposit}
+              onChange={(e) => setDeposit(e.currentTarget.value)}
+            />
+            <br />
+            <button
+              type="submit"
+              className="btn btn-light"
+              onClick={handleDeposit}
+            >
+              Deposit
+            </button>
+          </>
+        ) : (
+          <>
+            <h5>Success</h5>
+            <button type="submit" className="btn btn-light" onClick={clearForm}>
+              Deposit More Money
+            </button>
+          </>
+        )
+      }
+    />
   )
 }
--- withdraw.js
+++ withdraw.js
@@ -1,5 +1,67 @@
-function Withdraw(){
+function Withdraw() {
+  const [show, setShow] = React.useState(true)
+  const [status, setStatus] = React.useState('')
+  const [balance, setBalance] = React.useState('100')
+  const [withdraw, setWithdraw] = React.useState('')
+  const ctx = React.useContext(UserContext)
+
+  function validate(field, label) {
+    if (!field) {
+      setStatus('Error: ' + label)
+      setTimeout(() => setStatus(''), 3000)
+      return false
+    }
+    return true
+  }
+
+  function handleWithdraw() {
+    if (!validate(withdraw, 'withdraw')) return
+    ctx.users.push({ balance: 100 })
+    setShow(false)
+  }
+
+  function clearForm() {
+    setWithdraw('')
+    setShow(true)
+  }
+
   return (
-    <h1>Withdraw</h1>
+    <Card
+      bgcolor="primary"
+      header="Withdraw"
+      status={status}
+      body={
+        show ? (
+          <>
+            Balance: {balance}
+            <br />
+            <br />
+            <input
+              type="input"
+              className="form-control"
+              id="withdraw"
+              placeholder="Withdraw Amount"
+              value={withdraw}
+              onChange={(e) => setWithdraw(e.currentTarget.value)}
+            />
+            <br />
+            <button
+              type="submit"
+              className="btn btn-light"
+              onClick={handleWithdraw}
+            >
+              Withdraw
+            </button>
+          </>
+        ) : (
+          <>
+            <h5>Success</h5>
+            <button type="submit" className="btn btn-light" onClick={clearForm}>
+              Withdraw More Money
+            </button>
+          </>
+        )
+      }
+    />
   )
 }
