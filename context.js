d360deb78ea3ec0a13106167885901211e66727d
Mika Salazar <m.razalas@gmail.com>
2023-08-27 14:41:35 -0400

    add comments

--- context.js
+++ context.js
@@ -16,6 +16,7 @@ function Card(props) {
     <div className={classes()} style={{ maxWidth: '18rem' }}>
       <div className="card-header">{props.header}</div>
       <div className="card-body">
+        {/* conditional rendering - checks for their existence */}
         {props.title && <h5 className="card-title">{props.title}</h5>}
         {props.text && <p className="card-text">{props.text}</p>}
         {props.body}
